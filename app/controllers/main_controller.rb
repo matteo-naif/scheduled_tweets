20a8ba1ba447ca01ac44161c1bd25516ebb9b00c
Matteo Tortelli <matteo.tortelli@evoluzione.agency>
2023-05-19 15:56:23 +0200

    Login with Session Cookies

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -2,5 +2,8 @@ class MainController < ApplicationController
   def index
     # flash[:notice] = "Debug"
     # flash[:alert] = "Debug"
+    if session[:user_id]
+      @user = User.find_by(id: session[:user_id])
+    end
   end
 end
