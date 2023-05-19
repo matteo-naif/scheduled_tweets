a264d15832a99c49c17b466cb742e7edda3769ab
Matteo Tortelli <matteo.tortelli@evoluzione.agency>
2023-05-19 17:07:07 +0200

    Login Form

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,13 @@ Rails.application.routes.draw do
   get "sign_up", to: "registrations#new"
   # POST /sign_up
   post "sign_up", to: "registrations#create"
-  # GET /logout
+
+  # GET /sign_in
+  get "sign_in", to: "sessions#new"
+  # POST /sign_in
+  post "sign_in", to: "sessions#create"
+
+  # DELETE /logout
   delete "logout", to: "sessions#destroy"
   # GET /
   root to: "main#index"
